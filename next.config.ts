71c3044cae3ce662ba624d2d45bd16dc71cdee8e
Alan.Li <ykli109@outlook.com>
2025-01-03 01:35:02 +0800

    feat: use vercel to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    domains: ["itrade-web.t3techs.com"],
   },
 };
 
